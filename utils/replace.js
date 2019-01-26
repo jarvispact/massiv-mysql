c00850ef2e14cf0f5eb9743bdc53eb5e43292e3d
jarvispact <jarvispact@gmail.com>
2019-01-26 17:49:08 +0100

    replace falsy check with nil check

--- utils/replace.js
+++ utils/replace.js
@@ -1,5 +1,7 @@
+const isNil = value => value === undefined || value === null;
+
 module.exports = (query, db, ids = {}, params = {}) => query.replace(/:(\w+)/g, (txt, key) => {
-    if (ids[key]) return db.escapeId(ids[key]);
-    if (params[key]) return db.escape(params[key]);
+    if (!isNil(ids[key])) return db.escapeId(ids[key]);
+    if (!isNil(params[key])) return db.escape(params[key]);
     return txt;
 });
